6548451af7f4e0b543e7e0312ba2d9049b406dc2
arshkhatwani <arsh.khatwani@gmail.com>
2023-11-11 12:22:53 +0530

    Adds function to get images by album id

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
+const API_URL = "https://jsonplaceholder.typicode.com/photos";
+
+async function getImages(albumId) {
+    const { data } = await axios.get(API_URL, { params: { albumId } });
+    return data;
+}
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.get('/images', (req, res) => {
-    res.send("Images!");
-})
+app.get("/images", async (req, res) => {
+    const albumId = req.query.albumId != undefined ? req.query.albumId : 1;
+    const data = await getImages(albumId);
+    res.send(data);
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
